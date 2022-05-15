5d118a710f98c9cca3de0fdf4439e148c09f6100
krakenBDawg <brandonfurman9000@gmail.com>
2022-05-14 22:26:57 -0400

    Saturday 🤢
    the day, wasted. at least the thing is coded tho!

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function cardChange() {
   const elements = document.getElementsByClassName('inner');
   for (i in elements) {
     if (i > -1) {
-      elements[i].style.backgroundImage = "url('images/game-designs (" + (Math.ceil(Math.random() *  14) - 1) + ").png')";
+      elements[i].style.backgroundImage = "url('images/game-designs (" + (Math.ceil(Math.random() *  22) - 1) + ").png')";
     };
   };
   setTimeout(function () {
