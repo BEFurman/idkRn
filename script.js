fb466be80bfa8a4568d2b62fdc1539c67e9f5b0a
krakenBDawg <brandonfurman9000@gmail.com>
2022-05-17 21:15:33 -0400

    chewsday
    this is probably literally all garbage on a stick

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+import { app, db } from './firebase.js';
+import { collection, addDoc } from 'firebase/firestore';
+
 function cardChange() {
   const elements = document.getElementsByClassName('inner');
   for (i in elements) {
     if (i > -1) {
-      elements[i].style.backgroundImage = "url('images/game-designs (" + (Math.ceil(Math.random() *  22) - 1) + ").png')";
+      elements[i].style.backgroundImage = "url('images/game-designs (" + (Math.ceil(Math.random() * 24) - 1) + ").png')";
     };
   };
   setTimeout(function () {
@@ -14,3 +17,24 @@ function logIn() {
   document.getElementById('hidden').style.bottom = '0%';
 }
 cardChange();
+
+function submitButton() {
+  var submittedUn = document.getElementById('logInUn');
+  var submittedPw = document.getElementById('logInPw');
+  let deets = document.getElementsByClassName('deets');
+  for (element in deets) {
+    element.value = '';
+  }
+  const dbSubmit = () => {
+    addDoc(loginInfo, {
+      username: submittedUn,
+      password: submittedPw
+    })
+    .then(() => {
+      alert('Data Added! :D');
+    })
+    .catch((err) => {
+      alert(err.message);
+    })
+  }
+}
